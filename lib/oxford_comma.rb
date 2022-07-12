ab564483ce36523269805ccec25d21385ff0f516
loren <lorenmichaelh@gmail.com>
2022-07-12 16:07:32 -0500

    completed assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+    case array.length
+    when 1
+        array[0]
+    when 2
+        array.join(' and ')
+    when 3..100
+        array[-1] = "and #{array[-1]}"
+        array.join(', ')
+    end
 end
